36311dd58856a0ee197af9ee52df53b8e288044c
Julio <jcadriaz@uc.cl>
2013-09-03 10:53:32 -0400

    Inicio Tarea 1

---
+++ Tarea1/Process.py
@@ -0,0 +1,14 @@
+﻿
+
+class Process():
+
+	def __init__(self,inputText):
+		self._arrive_time = len(inputText)
+		# Aquí se debiera analizar todo el input del texto 
+		# y pasarlo a las distintas variables de interés
+
+	def getArriveTime(self):
+		return self._arrive_time
+
+a=Process("Holiiafdaií")
+print a.getArriveTime()
